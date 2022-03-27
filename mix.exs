d46e94e5265c8e5b818a278457c0035da3dd3fd1
Cory Finger <cory.j.finger@gmail.com>
2022-03-27 11:45:35 -0700

    Published hex package

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Dopple.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      name: "Dopple",
+      description: "A library that pings services and measures the results",
+      source_url: "https://github.com/polymorfiq-tools/dopple"
     ]
   end
 
@@ -25,6 +29,17 @@ defmodule Dopple.MixProject do
       {:uuid, "~> 1.1"},
       {:httpoison, "~> 1.8"},
       {:mox, "~> 0.5", only: [:test, :dev]}
+    ]
+  end
+
+  defp package do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "dopple",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE* CHANGELOG*),
+      licenses: ["AGPL-3.0-or-later"],
+      links: %{"GitHub" => "https://github.com/polymorfiq-tools/dopple"}
     ]
   end
 end
--- test/dopple_test.exs
+++ test/dopple_test.exs
@@ -11,8 +11,8 @@ defmodule DoppleTest do
     agg = Aggregator.Callback.new(fn evt -> send(test_pid, evt) end)
 
     with {:ok, m} <- Measurement.add_schedule(m, schedule),
-         {:ok, m} <- Measurement.add_target(m, target),
-         {:ok, m} <- Aggregator.add_measurement(agg, m) do
+         {:ok, _} <- Measurement.add_target(m, target),
+         {:ok, _} <- Aggregator.add_measurement(agg, m) do
       schedule |> Schedule.Event.enqueue({:test, "This is a test"})
       :timer.sleep(500)
     end
