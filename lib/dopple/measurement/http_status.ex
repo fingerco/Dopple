b499e32c2b2757fa6dec42d5700071dcef785a6b
Cory Finger <cory.j.finger@gmail.com>
2022-03-27 23:55:27 -0700

    Test credo warning

--- lib/dopple/measurement/http_status.ex
+++ lib/dopple/measurement/http_status.ex
@@ -3,7 +3,7 @@ defmodule Dopple.Measurement.Property do
     A measurement that retrieves a property of a response object
   """
   use GenStage
-  alias Dopple.{Target, Schedule}
+  alias Dopple.{Schedule, Target}
 
   @enforce_keys [:stage]
   defstruct [:stage, schedules: [], targets: [], id: UUID.uuid4()]
